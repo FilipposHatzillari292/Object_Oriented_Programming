ede004e48e857c1fa9b173423c05209422439aba
Filippos Hatzillari <fhaxhillari@nyc.gr>
2025-05-26 14:21:10 +0300

    Update APIClient.java

--- src/Assingment2/WordAPI.java
+++ src/Assingment2/WordAPI.java
@@ -3,13 +3,21 @@ package Assingment2;
 import java.io.*;
 import java.net.*;
 
-class WordAPIClient {
-    public static String getRandomWord() throws IOException {
-        URL url = new URL("https://random-word-api.herokuapp.com/word");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line = reader.readLine();
-        String s=line.replaceAll ( "[\\[]\"]", "" );
-        return s;
+public class APIClient {
+    public static String getRandomWord() {
+        try {
+            URL url = new URL("https://random-word-api.herokuapp.com/word?number=1");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = reader.readLine(); // e.g., ["hangman"]
+            reader.close();
+
+            return line.substring(2, line.length() - 2); // Extract word from ["word"]
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "default";
+        }
     }
 }
